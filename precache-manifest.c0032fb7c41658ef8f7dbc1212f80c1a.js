53175c73614459163603d502d10c7ace40bb1514
Amit Maurya <zebrapayment123@gmail.com>
2020-04-12 01:43:25 +0530

    Updates

---
+++ precache-manifest.c0032fb7c41658ef8f7dbc1212f80c1a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64f27ba3ed3fe9d63f3f213483bb0cdb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8d7af364f58ae29bde0",
+    "url": "/static/css/2.bf0413c3.chunk.css"
+  },
+  {
+    "revision": "bf54b08a43a24346b3e8",
+    "url": "/static/css/main.66da6dbd.chunk.css"
+  },
+  {
+    "revision": "f8d7af364f58ae29bde0",
+    "url": "/static/js/2.f7c3a698.chunk.js"
+  },
+  {
+    "revision": "a55f143b1897642c8dd1ebb02104c7de",
+    "url": "/static/js/2.f7c3a698.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf54b08a43a24346b3e8",
+    "url": "/static/js/main.63b57fa7.chunk.js"
+  },
+  {
+    "revision": "eb2da8a6912a8c64fb47",
+    "url": "/static/js/runtime-main.555d898a.js"
+  },
+  {
+    "revision": "aea905417b21e6d3e64419c2376b754f",
+    "url": "/static/media/Resume_Amit_Maurya.aea90541.pdf"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
